5221800776655221856a3cec36d1a2c3a415583d
Yulia-Kot <yuhalaniuk@ukr.net>
2023-06-05 21:44:00 +0300

    added player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
+
+player.on('timeupdate', function (evt) {
+  console.log(evt.seconds);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+});
