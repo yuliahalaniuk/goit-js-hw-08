1145ced8c10c8d6210a651f626053394f30434e8
Yulia-Kot <yuhalaniuk@ukr.net>
2023-06-08 11:29:23 +0300

    fixed form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,42 @@
 import _throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
 const feedbackFormRef = document.querySelector('.feedback-form');
 const textareaRef = document.querySelector('.feedback-form textarea');
 const emailInputRef = document.querySelector('.user-email');
 
-const STORAGE_KEY = 'feedback-form-state';
-
-let settings = {};
+// let settings = {};
 fillFormFields();
 
-feedbackFormRef.addEventListener('submit', onFormSubmit);
 feedbackFormRef.addEventListener('input', _throttle(onFormInput, 500));
+feedbackFormRef.addEventListener('submit', onFormSubmit);
 
 function onFormInput({ target }) {
-  //   if (target.nodeName === 'INPUT') {
-  //     settings.email = target.value;
-  //   } else if (target.nodeName === 'TEXTAREA') {
-  //     settings.message = target.value;
-  //   }
+  // settings[target.name] = target.value;
+  const formUserData = {
+    email: emailInputRef.value,
+    message: textareaRef.value,
+  };
 
-  settings[target.name] = target.value;
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formUserData));
 }
 
 function fillFormFields() {
   const savedInfo = localStorage.getItem(STORAGE_KEY);
 
-  if (savedInfo) {
-    const { message, email } = JSON.parse(savedInfo);
-    textareaRef.value = message;
-    emailInputRef.value = email;
-  }
+  if (!savedInfo) return;
+
+  const { message, email } = JSON.parse(savedInfo);
+  textareaRef.value = message;
+  emailInputRef.value = email;
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
+  if (!textareaRef.value || !emailInputRef.value) {
+    return alert('All feels must be fell out!');
+  }
   console.log(localStorage.getItem(STORAGE_KEY));
   localStorage.removeItem(STORAGE_KEY);
   evt.currentTarget.reset();
